91075206393a943b3d9ac6d338d5785f922113f8
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2024-12-11 00:26:18 +0200

    Update books_routes.php

--- books_routes.php
+++ books_routes.php
@@ -10,6 +10,6 @@ Route::get('/', function () {
 Route::get('/books', [BookController::class, 'index'])->name('books');
 Route::get('/books/create', [BookController::class, 'create'])->name('books_create');
 Route::post('/books', [BookController::class, 'store'])->name('books_store');
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books_edit');
-Route::put('/books/{book}', [BookController::class, 'update'])->name('books_update');
-Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books_destroy');
+Route::get('/books/edit/{id}', [BookController::class, 'edit'])->name('books_edit');
+Route::put('/books/update/{id}', [BookController::class, 'update'])->name('books_update');
+Route::delete('/books/destroy/{id}', [BookController::class, 'destroy'])->name('books_destroy');
