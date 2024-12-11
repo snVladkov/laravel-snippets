8e28ff558d9121dcb349d10454636c6d9f44da36
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2024-12-11 15:05:05 +0200

    Update index.blade.php

--- books/index.blade.php
+++ books/index.blade.php
@@ -1,16 +1,31 @@
 <x-layout>
-{{-- таблица с книги --}}
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Заглавие</th>
-            <th>Автор</th>
-            <th>Година</th>
-            <th>Действия</th>
-        </tr>
-    </thead>
-    <tbody>
-        {{-- Редове с книги --}}
-    </tbody>
-</table>
+    {{-- таблица с книги --}}
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Заглавие</th>
+                <th>Автор</th>
+                <th>Година</th>
+                <th>Действия</th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- Редове с книги --}}
+            @foreach ($books as $book)
+                <tr>
+                    <td>{{ $book['title'] }}</td>
+                    <td>{{ $book['author'] }}</td>
+                    <td>{{ $book['year'] }}</td>
+                    <td>
+                        <a href="{{ route('books_edit', $book['id']) }}" class="btn btn-warning">Редактирай</a>
+                        <form class="d-inline" method="POST" action="{{ route('books_destroy', $book['id']) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Изтрий</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-layout>
