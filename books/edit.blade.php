e0031f3a8dffd034a06394f74db636db647948e5
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2024-12-11 15:04:44 +0200

    Update edit.blade.php

--- books/edit.blade.php
+++ books/edit.blade.php
@@ -1,17 +1,28 @@
 <x-layout>
     {{-- форма за редактиране на книга --}}
-    <form class="border rounded p-4 w-50 mx-auto">
+    <form class="border rounded p-4 w-50 mx-auto" method="POST" action="{{ route('books_update', $book['id']) }}">
+        @csrf
+        @method('PUT')
         <div class="mb-3">
             <label for="title" class="form-label">Заглавие</label>
-            <input type="text" class="form-control" id="title">
+            <input type="text" class="form-control" id="title" name="title" value="{{ $book['title'] }}">
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="author" class="form-label">Автор</label>
-            <input type="text" class="form-control" id="author">
+            <input type="text" class="form-control" id="author" name="author" value="{{ $book['author'] }}">
+            @error('author')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="year" class="form-label">Година</label>
-            <input type="number" class="form-control" id="year">
+            <input type="number" class="form-control" id="year" name="year" value="{{ $book['year'] }}">
+            @error('year')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
         <button type="submit" class="btn btn-success">Редактирай</button>
     </form>
